675b764361a15c1a969c4aac96e1ed3833068a65
Olucho <luchoorfino@gmail.com>
2021-03-06 11:14:01 -0300

    Add user entity

---
+++ backend/src/user/user.entity.ts
@@ -0,0 +1,38 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  bio: string;
+
+  @Column()
+  avatar: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
--- backend/src/user/user.repository.ts
+++ backend/src/user/user.repository.ts
@@ -1,4 +1,4 @@
 import { EntityRepository, Repository } from 'typeorm';
-
+import { User } from './user.entity';
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {}
