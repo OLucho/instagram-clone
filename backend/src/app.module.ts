e6512b0a61ef7dbb0676104fe5f8bdf088568d6b
Olucho <luchoorfino@gmail.com>
2021-03-08 14:59:54 -0300

    Add photo model setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfig } from './config/db.config';
 import { UserModule } from './user/user.module';
+import { PhotoModule } from './photo/photo.module';
 @Module({
-  imports: [TypeOrmModule.forRoot(TypeOrmConfig), UserModule],
+  imports: [TypeOrmModule.forRoot(TypeOrmConfig), UserModule, PhotoModule],
   controllers: [],
   providers: [],
 })
