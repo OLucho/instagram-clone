c44a6ba58c5b61fb8a8230ed5583ffc5f6c157a9
Olucho <luchoorfino@gmail.com>
2021-03-08 01:09:46 -0300

    Injects JWT

---
+++ backend/src/user/jwt/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { UserRepository } from '../user.repository';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UserRepository) private userRepository: UserRepository,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+  async validate({ username }) {
+    const user = await this.userRepository.findOne({ username });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,14 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { JwtService } from '@nestjs/jwt';
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
 import { fromDtoToEntity } from './user.mapper';
 import { ValidationUserDto } from './dto/create.user.dto';
+import { SignInDto } from './dto/signIn.dto';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(UserRepository) private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
   async createUser(validationUserDto: ValidationUserDto): Promise<User> {
@@ -18,5 +21,22 @@ export class UserService {
 
   async isUserAlreadyCreated(username: string, email: string): Promise<User> {
     return this.userRepository.isUserAlreadyCreated(username, email);
+  }
+
+  async signIn(signInDto: SignInDto) {
+    const { password } = signInDto;
+    const user = await this.userRepository.signIn(signInDto);
+    if (!user) {
+      throw new BadRequestException('User Incorrect');
+    }
+    if (password !== user.password) {
+      throw new BadRequestException('Password Incorrect');
+    }
+
+    const { username } = user;
+    const payload = { username };
+    const accessToken = this.jwtService.sign(payload);
+
+    return { accessToken };
   }
 }
