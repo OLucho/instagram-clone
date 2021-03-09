6d824eb5448400a5a55053cccd7d2eb0c2830c9b
Olucho <luchoorfino@gmail.com>
2021-03-09 15:38:51 -0300

    Injects User and Photo Modules in Comments one

--- backend/src/comment/comment.module.ts
+++ backend/src/comment/comment.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PhotoModule } from 'src/photo/photo.module';
+import { UserModule } from 'src/user/user.module';
 import { CommentController } from './comment.controller';
 import { CommentRepository } from './comment.repository';
 import { CommentService } from './comment.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CommentRepository])],
+  imports: [
+    TypeOrmModule.forFeature([CommentRepository]),
+    PhotoModule,
+    UserModule,
+  ],
   controllers: [CommentController],
   providers: [CommentService],
 })
