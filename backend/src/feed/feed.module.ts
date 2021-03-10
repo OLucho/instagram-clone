efdeb25fab0d0d0bbc5fd43021bb67aee2cbd354
Olucho <luchoorfino@gmail.com>
2021-03-10 12:39:38 -0300

    Injects User and Photo Modules in Feeds one

--- backend/src/feed/feed.module.ts
+++ backend/src/feed/feed.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PhotoModule } from 'src/photo/photo.module';
+import { UserModule } from 'src/user/user.module';
 import { FeedController } from './feed.controller';
 import { FeedService } from './feed.service';
 
 @Module({
+  imports: [UserModule, PhotoModule],
   controllers: [FeedController],
-  providers: [FeedService]
+  providers: [FeedService],
 })
 export class FeedModule {}
