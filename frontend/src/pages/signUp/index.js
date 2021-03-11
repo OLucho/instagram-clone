87dd4e1a71c5ba949a3214a6b0d65384f0924507
Olucho <luchoorfino@gmail.com>
2021-03-11 00:30:55 -0300

    Add SignUp page layout

--- frontend/src/pages/signUp/index.js
+++ frontend/src/pages/signUp/index.js
@@ -1,3 +1,34 @@
-export default function signUp() {
-  return <h1>from signUp</h1>;
+import { Link } from 'react-router-dom';
+import { Container, Footer, Form, FormContainer, Gif } from './styles';
+import gif from './styles/gif.gif';
+import logo from '../../assets/logo.png';
+
+export default function Signup() {
+  return (
+    <Container>
+      <Gif src={gif} alt="gif" />
+
+      <FormContainer>
+        <Form>
+          <img src={logo} alt="logo" />
+          <span>Register to upload images </span>
+          <hr />
+          <input value="" name="" />
+
+          <button type="submit">Register</button>
+          <hr />
+          <span className="footer">
+            By registering you accept our conditions, cookie policies and
+            services
+          </span>
+        </Form>
+
+        <Footer>
+          <p>
+            Already have an account? <Link to="signin">Sign In!</Link>
+          </p>
+        </Footer>
+      </FormContainer>
+    </Container>
+  );
 }
