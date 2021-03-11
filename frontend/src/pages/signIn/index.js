105fc31fd730fa1ade6382ff8c9be49d09d1d0f5
Olucho <luchoorfino@gmail.com>
2021-03-11 12:29:51 -0300

    Add SignIn page layout

--- frontend/src/pages/signIn/index.js
+++ frontend/src/pages/signIn/index.js
@@ -1,3 +1,69 @@
-export default function SignIn() {
-  return <h1>from SignIn</h1>;
+/* eslint-disable react/no-unescaped-entities */
+import { Link, useHistory } from 'react-router-dom';
+import { useRef, useState } from 'react';
+import * as Yup from 'yup';
+import { Container, Footer, Form, FormContainer, ErrorMessage } from './styles';
+import Input from '../../components/input';
+import logo from '../../assets/logo.png';
+import { getValidationErrors } from '../../utils/validation';
+
+export default function Signup() {
+  const formRef = useRef(null);
+  const history = useHistory();
+  const [serverError, setServerError] = useState('');
+
+  const handleSubmit = async (data) => {
+    try {
+      formRef.current.setErrors([]);
+      const schema = Yup.object().shape({
+        username: Yup.string().required('Username is Required').max(8),
+        password: Yup.string().required('Password is Required').min(5),
+      });
+      await schema.validate(data, { abortEarly: false });
+      history.push('/');
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(error);
+        formRef.current.setErrors(errors);
+      }
+      if (error.response) {
+        setServerError(error.response.data.message);
+      }
+    }
+  };
+
+  return (
+    <Container>
+      <FormContainer>
+        <Form onSubmit={handleSubmit} ref={formRef}>
+          <img src={logo} alt="logo" />
+          <span>Log in to watch photos and videos </span>
+          <hr />
+          {serverError && (
+            <ErrorMessage>
+              <p>{serverError}</p>
+            </ErrorMessage>
+          )}
+          <Input name="username" placeholder="Enter your username" />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+          />
+          <button type="submit">Register</button>
+          <hr />
+
+          <span className="footer">
+            Watch what your friends have prepared for you
+          </span>
+        </Form>
+
+        <Footer>
+          <p>
+            Don't have an Account yet? <Link to="signup">Sign Up!</Link>
+          </p>
+        </Footer>
+      </FormContainer>
+    </Container>
+  );
 }
