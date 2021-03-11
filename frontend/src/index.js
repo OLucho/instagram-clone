b3b8083ee2c3dc1a553c6f3e5ddd78dfee4c9e9f
Olucho <luchoorfino@gmail.com>
2021-03-11 00:00:55 -0300

    Installs ESLint(Airbnb config) and Prettier + Format

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import globalStyles from "./globalStyles";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import globalStyles from './globalStyles';
 
 ReactDOM.render(
   <React.StrictMode>
     <globalStyles />
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
