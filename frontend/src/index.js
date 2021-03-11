8c11d31787a9b549685dcb4a2002331d95fc8872
Olucho <luchoorfino@gmail.com>
2021-03-10 23:50:29 -0300

    Add global styles

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import globalStyles from "./globalStyles";
 
 ReactDOM.render(
   <React.StrictMode>
+    <globalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
