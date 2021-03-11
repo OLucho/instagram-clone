cf0fbf582d17f197634d7e6fe5b8707929b7d4d8
Olucho <luchoorfino@gmail.com>
2021-03-11 15:22:57 -0300

    Add Upload Hook

---
+++ frontend/src/hooks/upload.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-undef */
+import { createContext, useContext, useCallback, useState } from 'react';
+import api from '../api';
+
+const UploadContext = createContext();
+
+export function UploadProvider({ children }) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const uploadPhotoAction = useCallback(async (dataImage) => {
+    try {
+      const formData = new FormData();
+      formData.append('file', dataImage.file, dataImage.file.name);
+      formData.append('body', dataImage.body);
+      const res = await api.post('/photo/upload', formData);
+
+      if (res.status === 200) {
+        setData(res);
+      }
+    } catch (err) {
+      if (err.response.status === 500) {
+        setError(true);
+      }
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  const resetValues = useCallback(() => {
+    setData(null);
+    setLoading(false);
+    setError(false);
+  }, []);
+
+  return (
+    <UploadContext.Provider
+      value={{ data, loading, error, uploadPhotoAction, resetValues }}
+    >
+      {children}
+    </UploadContext.Provider>
+  );
+}
+
+export function useUpload() {
+  const context = useContext(UploadContext);
+  if (!context) {
+    throw new Error('useUpload must be used within a UploadProvider  ');
+  }
+  return context;
+}
