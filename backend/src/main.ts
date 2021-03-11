7985ec829d387a63369457ab3695292922dca451
Olucho <luchoorfino@gmail.com>
2021-03-10 23:40:18 -0300

    Enable CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(process.env.PORT);
 }
 bootstrap();
