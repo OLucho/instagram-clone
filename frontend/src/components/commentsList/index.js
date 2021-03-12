88dfd75c8f195cb8258c3af12bd76b80451645c7
Olucho <luchoorfino@gmail.com>
2021-03-11 23:15:17 -0300

    Add CommentsList component

---
+++ frontend/src/components/commentsList/index.js
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+import englishString from 'react-timeago/lib/language-strings/en';
+import buildFormatter from 'react-timeago/lib/formatters/buildFormatter';
+import TimeAgo from 'react-timeago';
+import { Comment, Container, StylesTimeAgo } from './styles';
+
+export default function CommentList({ comments }) {
+  const formatter = buildFormatter(englishString);
+
+  return (
+    <Container>
+      {comments &&
+        comments.map((comment) => (
+          <Comment key={comment.id}>
+            <div>
+              <Link to={`/profile/${comment.user.username}`}>
+                {comment.user.username}
+              </Link>
+              <span>{comment.body}</span>
+            </div>
+
+            <StylesTimeAgo>
+              <TimeAgo date={`${comment.created_at}Z`} formatter={formatter} />
+            </StylesTimeAgo>
+          </Comment>
+        ))}
+    </Container>
+  );
+}
