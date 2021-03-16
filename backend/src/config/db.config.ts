a765d6aa3393a6793ec094e5ac432c2bc8db6c07
Olucho <luchoorfino@gmail.com>
2021-03-16 12:27:08 -0300

    Changes for deploy

--- backend/src/config/db.config.ts
+++ backend/src/config/db.config.ts
@@ -2,9 +2,9 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const TypeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: process.env.DB_HOST,
   port: 5432,
-  username: 'postgres',
+  username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   entities: [__dirname + '/../**/*.entity.js'],
--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
+let baseURL;
+if (process.env.NODE_ENV === 'production') {
+  baseURL = 'https://smarket-cpr.herokuapp.com/';
+} else {
+  baseURL = 'http://localhost:4000';
+}
+
 const api = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL,
 });
 
 export default api;
